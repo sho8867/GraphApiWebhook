6054d73b994f34f11ec50d2bd02eb4dcce882222
kyoku <kyoku@DESKTOP-PUAJ3BV>
2023-04-03 13:46:52 +0900

    プロジェクト ファイルを追加します。

---
+++ FunctionApp1/Function1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation(req.Path.Value);
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation(requestBody);
+
+            Microsoft.Extensions.Primitives.StringValues queryVal;
+            if (req.Query.TryGetValue("validationToken", out queryVal))
+            {
+                var token = queryVal.ToString();
+                log.LogInformation("validationToken include ");
+                return new OkObjectResult(token);
+            }
+
+            var notifications = JsonConvert.DeserializeObject<Notifications>(requestBody);
+            if(notifications.Items.Count() == 0)
+            {
+                log.LogInformation("value not found");
+                return new OkObjectResult("OK");
+            }
+
+            // 対称キーを復号する
+            RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider();
+            RSAParameters param = RsaUtil.CreateParameter("<RSAKey"); // RSA key
+            rsaProvider.ImportParameters(param);
+
+            foreach (var notification in notifications.Items)
+            {
+                byte[] encryptedSymmetricKey = Convert.FromBase64String(notification.encryptedContent.DataKey);
+                byte[] decryptedSymmetricKey = rsaProvider.Decrypt(encryptedSymmetricKey, fOAEP: true);
+
+                // HMAC - SHA256 を使用してデータの署名を比較する
+                byte[] encryptedPayload = Convert.FromBase64String(notification.encryptedContent.Data);  //< the value from the data property, still encrypted>;
+                byte[] expectedSignature = Convert.FromBase64String(notification.encryptedContent.DataSignature);
+                byte[] actualSignature;
+
+                using (HMACSHA256 hmac = new HMACSHA256(decryptedSymmetricKey))
+                {
+                    actualSignature = hmac.ComputeHash(encryptedPayload);
+                }
+
+                if (actualSignature.SequenceEqual(expectedSignature))
+                {
+                    // リソース データ コンテンツを復号する
+                    AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider();
+                    aesProvider.Key = decryptedSymmetricKey;
+                    aesProvider.Padding = PaddingMode.PKCS7;
+                    aesProvider.Mode = CipherMode.CBC;
+                    //var aes = Aes.Create();
+                    //aes.Key = decryptedSymmetricKey;
+                    //aes.Padding = PaddingMode.PKCS7;
+                    //aes.Mode = CipherMode.CBC;
+
+                    // Obtain the intialization vector from the symmetric key itself.
+                    int vectorSize = 16;
+                    byte[] iv = new byte[vectorSize];
+                    Array.Copy(decryptedSymmetricKey, iv, vectorSize);
+                    aesProvider.IV = iv;
+
+                    string decryptedResourceData;
+                    // Decrypt the resource data content.
+                    using (var decryptor = aesProvider.CreateDecryptor())
+                    {
+                        using (MemoryStream msDecrypt = new MemoryStream(encryptedPayload))
+                        {
+                            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                            {
+                                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                                {
+                                    decryptedResourceData = srDecrypt.ReadToEnd();
+                                    log.LogInformation(decryptedResourceData);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    log.LogInformation("actualSignature unmatch");
+                }
+            }
+            return new OkObjectResult("OK");
+        }
+    }
+}
---
+++ FunctionApp1/Notification.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunctionApp1
+{
+
+    public class Notifications
+    {
+        [JsonProperty(PropertyName = "value")]
+        public Notification[] Items { get; set; }
+
+        [JsonProperty(PropertyName = "validationTokens")]
+        public string[] ValidationTokens { get; set; }
+    }
+    public class Notification
+    {
+        [JsonProperty(PropertyName = "changeType")]
+        public string ChangeType { get; set; }
+
+        [JsonProperty(PropertyName = "clientState")]
+        public string ClientState { get; set; }
+
+        [JsonProperty(PropertyName = "resource")]
+        public string Resource { get; set; }
+
+        [JsonProperty(PropertyName = "subscriptionExpirationDateTime")]
+        public DateTimeOffset SubscriptionExpirationDateTime { get; set; }
+
+        [JsonProperty(PropertyName = "subscriptionId")]
+        public string SubscriptionId { get; set; }
+
+        [JsonProperty(PropertyName = "resourceData")]
+        public ResourceData ResourceData { get; set; }
+
+        [JsonProperty(PropertyName = "encryptedContent")]
+        public EncryptedContent encryptedContent { get; set; }
+    }
+
+    public class ResourceData
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "@odata.type")]
+        public string ODataEType { get; set; }
+
+        [JsonProperty(PropertyName = "@odata.id")]
+        public string ODataId { get; set; }
+    }
+
+    public class EncryptedContent
+    {
+        [JsonProperty(PropertyName = "data")]
+        public string Data { get; set; }
+        
+        [JsonProperty(PropertyName = "dataSignature")]
+        public string DataSignature { get; set; }
+        
+        [JsonProperty(PropertyName = "dataKey")]
+        public string DataKey { get; set; }
+        
+        [JsonProperty(PropertyName = "encryptionCertificateId")]
+        public string EncryptionCertificateId { get; set; }
+
+        [JsonProperty(PropertyName = "encryptionCertificateThumbprint")]
+        public string EncryptionCertificateThumbprint { get; set; }
+    }
+}
